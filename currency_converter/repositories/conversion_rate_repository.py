3a6cb176b082405421f63c50b11e55333ee74a32
nicolequindara <cquindara@gmail.com>
2024-09-25 11:58:32 -0700

    feat: refresh in cache hourly

--- currency_converter/repositories/conversion_rate_repository.py
+++ currency_converter/repositories/conversion_rate_repository.py
@@ -1,5 +1,7 @@
 import csv
+from datetime import date, datetime, timedelta
 from decimal import Decimal
+from typing import Optional
 from currency_converter.domain import ConversionRate
 
 FILENAME = "currency_converter/repositories/Conversion Rates.csv"
@@ -7,8 +9,9 @@ FILENAME = "currency_converter/repositories/Conversion Rates.csv"
 class ConversionRateRepository:
     def __init__(self):
         self.rate_map = None
+        self.last_refresh = None
 
-    def get_all(self, force_refresh_cache: bool = False) -> dict[str, ConversionRate]:
+    def get_all(self, last_refresh: Optional[date] = None) -> dict[str, ConversionRate]:
         """
         Retrieve currency information
         Country Code, Currency, Conversion Rate from USD
@@ -19,7 +22,8 @@ class ConversionRateRepository:
         Returns:
             dictionary of country code to ConversionRate
         """
-        if force_refresh_cache or not self.rate_map:
+        should_refresh_cache = (last_refresh == None) or (last_refresh > datetime.utcnow() - timedelta(hours=1))
+        if should_refresh_cache:
             print("Retrieving conversion rate information from CSV")
             rate_map = {} # singleton map of country code to ConversionRate
             with open(FILENAME, mode="r") as csvfile:
@@ -32,6 +36,7 @@ class ConversionRateRepository:
                         conversion_rate=Decimal(row[2]),
                     )
             self.rate_map = rate_map
+            self.last_refresh = datetime.utcnow()
         return self.rate_map
 
 
